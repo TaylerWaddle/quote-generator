5df2ce09f30dad929aa80786a1811d73800cda79
Tayler Waddle <taylerwaddle@gmail.com>
2024-06-27 16:58:43 -0600

    Added DOM Manipultaion for quotes and authors

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from API quotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // Check Author for Anonymous, and empty authors
+  if (
+    quote.author === "Anonymous" ||
+    quote.author === "Anonymousk" ||
+    !quote.author
+  ) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote Length to Determine Styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
